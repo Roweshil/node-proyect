08b472acbc2b7ce50192b4999176054e87f0ee27
Roweshil <x-vdm@hotmail.com>
2026-01-12 14:32:12 -0600

    Remover node_modules and add dotenv to gitignore

---
+++ node-js-user-auth/user-repository.js
@@ -0,0 +1,13 @@
+import dbLocal from "db-local"
+const {Schema} = new dbLocal({path: './db'})
+
+const User = Schema('User', {
+    _id: {type: String, required:true },
+    username: {type: String, required:true},
+    password: {type: String, required:true}
+})
+
+export class UserRepository {
+    static create (username, password) {}
+    static login (username, password) {}
+}
