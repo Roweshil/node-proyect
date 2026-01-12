ec3941dffdff6545653089581f59304e7cbea759
Roweshil <x-vdm@hotmail.com>
2026-01-12 14:06:46 -0600

    Clase-Auth

---
+++ node-js-user-auth/README.md
@@ -0,0 +1,12 @@
+1. Instalacion del proyecto
+2. Usuarios en base de datos
+3. Registro de usuarios
+4. Autenticacion basica con login y password
+5. Verificacion de autenticacion
+6. Sesion de usuario con cookie
+7. JSON Web Tokens
+8. Rutas protegidas
+9. Refactorizar con un middleware
+10. Cerrar sesion
+11. Refresh Token
+12. ToDo Passport y OAuth 2.0
---
+++ node-js-user-auth/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { PORT } from './config.js'
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('<H1>Hello, World!</H1>')
+})
+
+app.post('/login', (req, res) => {
+  res.send('Login endpoint')
+})
+
+app.post('/register', (req, res) => {
+  res.send('Register endpoint')
+})
+
+app.post('/logout', (req, res) => {
+  res.send('Logout endpoint')
+})
+
+app.post('/protected', (req, res) => {
+  res.send('Protected endpoint')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
